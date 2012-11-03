950f147cc5eeb7d78f3b12e93ffed6328df73f9f
Matthew Harrison-Jones <matthojo@gmail.com>
2012-11-03 13:43:20 +0000

    Improved description

--- js/application.js
+++ js/application.js
@@ -1,14 +1,27 @@
-$.getJSON('GameData.json', function(data) {
+$(document).ready(function() {
+
+  $.getJSON('GameData.json', function(data) {
     var template = $('#gameTemplate').html();
     var html = Mustache.to_html("{{%IMPLICIT-ITERATOR}}\n"+template, data);
     $('#GamesList').html(html);
+
+   	init();
+	});
+
 });
 
-$(".cat_select ul li a").on("click", function(){
-	var hash = $(this).attr("href").replace('#', '');
-	if(hash != "") showOnlyCat(hash);
-	else showAllCat();
-});
+function init(){
+	var hash = window.location.hash.replace('#', '');
+  	if(hash) showOnlyCat(hash);
+
+	$(".cat_select ul li a").on("click", function(){
+		var hash = $(this).attr("href").replace('#', '');
+		if(hash != "") showOnlyCat(hash);
+		else showAllCat();
+	});
+
+	$(".game_item .description[rel=popover]").popover({trigger: "hover", placement:"right", title:"Description"});
+}
 
 function showOnlyCat(className){
  $("#GamesList ul .game_item").hide();
